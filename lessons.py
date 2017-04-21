4189373b6195606fde638125cc58af5b92aa90eb
Ignacio Rodriguez <ignacio@sugarlabs.org>
2017-04-21 02:01:24 -0400

    Initial commit

---
+++ lessons.py
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+from gettext import gettext as _
+import glib
+
+LESSONS = (
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 9 (Part 1)')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Rule #8: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('Sharing makes room for us to receive more.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Pickup Points:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Although it seems we have less when we share, we actually get more.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Sharing is a tool to overcome our reactive behaviors and receive the Spiritual fulfillment we really want.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Sharing is being like the Light.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Everybody has something to share; we can share both physical things and our unique Light-qualities.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n' + \
+        glib.markup_escape_text(_('   • What is one positive outcome of sharing you have experienced?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Explain what the following phrase means to you: “The more you give, the more you get.”')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Write down three sharing actions you did this week. How did it make you feel?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • How does your Opponent Voice challenge you when you want to share? What does it tell you?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • In what situations do you have a hard time sharing?')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 9 (Part 2)')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Rule #8: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('Sharing makes room for us to receive more.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Pickup Points:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Although it seems we have less when we share, we actually get more.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Sharing is a tool to overcome our reactive behaviors and receive the Spiritual fulfillment we really want.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Sharing is being like the Light.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Everybody has something to share; we can share both physical things and our unique Light-qualities.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n' + \
+        glib.markup_escape_text(_('   • What is one positive outcome of sharing you have experienced?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Explain what the following phrase means to you: “The more you give, the more you get.”')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Write down three sharing actions you did this week. How did it make you feel?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • How does your Opponent Voice challenge you when you want to share? What does it tell you?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • In what situations do you have a hard time sharing?')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 10 (Part 1)')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Rule #9: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('True sharing means sharing with love; to help and without expectations.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Pickup Points:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • True sharing is doing what’s best for me, for others and the world.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • By getting to know our friends, family and those around us better, we can learn what and how to share with them.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • To truly share , we must do three things:')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       1. share with love')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       2. share to help')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       3. share without expectations')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Words to Know:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Share with Love: Choosing to share with kindness, care and a positive attitude – not because we have to.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Share to Help: Identifying what the other person needs and making sure our actions assists rather than harm; sharing doesn’t always mean giving.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Share without Expectations: Sharing without looking to receive something in return, knowing that you always get spiritual fulfillment.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • In your own words, explain what the three components of sharing mean to you:')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       1. Share with love means…')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       2. Share to help means…')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       3. Share without expectations means…')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Write the items from your list of which you feel you need to start doing more. For example: Exercise, read, expressing my feelings when I’m angry, etc.')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 10 (Two Brothers)')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Rule #9: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('True sharing means sharing with love; to help and without expectations.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Pickup Points:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • True sharing is doing what’s best for me, for others and the world.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • By getting to know our friends, family and those around us better, we can learn what and how to share with them.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • To truly share , we must do three things:')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       1. share with love')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       2. share to help')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       3. share without expectations')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Words to Know:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Share with Love: Choosing to share with kindness, care and a positive attitude – not because we have to.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Share to Help: Identifying what the other person needs and making sure our actions assists rather than harm; sharing doesn’t always mean giving.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Share without Expectations: Sharing without looking to receive something in return, knowing that you always get spiritual fulfillment.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • In your own words, explain what the three components of sharing mean to you:')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       1. Share with love means…')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       2. Share to help means…')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       3. Share without expectations means…')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Write the items from your list of which you feel you need to start doing more. For example: Exercise, read, expressing my feelings when I’m angry, etc.')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 10 (Part 2)')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Rule #9: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('True sharing means sharing with love; to help and without expectations.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Pickup Points:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • True sharing is doing what’s best for me, for others and the world.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • By getting to know our friends, family and those around us better, we can learn what and how to share with them.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • To truly share , we must do three things:')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       1. share with love')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       2. share to help')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       3. share without expectations')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Words to Know:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Share with Love: Choosing to share with kindness, care and a positive attitude – not because we have to.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Share to Help: Identifying what the other person needs and making sure our actions assists rather than harm; sharing doesn’t always mean giving.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Share without Expectations: Sharing without looking to receive something in return, knowing that you always get spiritual fulfillment.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • In your own words, explain what the three components of sharing mean to you:')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       1. Share with love means…')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       2. Share to help means…')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       3. Share without expectations means…')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Write the items from your list of which you feel you need to start doing more. For example: Exercise, read, expressing my feelings when I’m angry, etc.')),
+)
---
+++ videos.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+VIDEOS = (
+    ("9.1.ogv",
+     _("The Power of Sharing (Part 1)"),
+     _("Rule #8: Sharing makes room for us to receive more.\n\
+Although it seems we have less when we share, we actually get more.\n\
+Sharing is a tool to overcome our reactive behaviors and receive the Spiritual fulfillment we really want.\n\
+Sharing is being like the Light.")
+         ),
+    ("9.5.ogv",
+     _("The Power of Sharing (Part 2)"),
+     _("Rule #8: Sharing makes room for us to receive more.\n\
+Although it seems we have less when we share, we actually get more.\n\
+Sharing is a tool to overcome our reactive behaviors and receive the Spiritual fulfillment we really want.\n\
+Sharing is being like the Light.")
+         ),
+    ("10.1.ogv",
+     _("How to Share (Part 1)"),
+     _("Rule #9: True sharing means sharing with love; to help and without expectations.\n\
+True sharing is doing what’s best for me, for others and the world.\n\
+By getting to know our friends, family and those around us better, we can learn what and how to share with them."),
+    ),
+    ("10.2.ogv",
+     _("Two Brothers"),
+     _("Rule #9: True sharing means sharing with love; to help and without expectations.\n\
+True sharing is doing what’s best for me, for others and the world.\n\
+By getting to know our friends, family and those around us better, we can learn what and how to share with them."),
+    ),
+    ("10.6.ogv",
+     _("How to Share (Part 2)"),
+     _("Rule #9: True sharing means sharing with love; to help and without expectations.\n\
+True sharing is doing what’s best for me, for others and the world.\n\
+By getting to know our friends, family and those around us better, we can learn what and how to share with them."),
+    ),
+)
